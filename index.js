caf7db40c644cbf199246d71055fceae52ec2b2e
laloter118 <lalin275@outlook.es>
2021-07-11 11:08:30 -0500

    Agregando funciones al index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const path = require ('path');
+const mongoose = require ('mongoose');
+const cors = require('cors');
+
+const app=express();
+
+
+mongoose.Promise=global.Promise;
+const dbURL = 'mongodb://localhost:27017/';
+mongoose.connect(dbURL, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false})
+.then(mongoose=>console.log('Conectado en el servidor de bases de datos de mongodb puerto'))
+.catch (err=>console.log(err));
+
+app.set('port', process.env.PORT || 4000 );
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname,'public')))
+
+app.listen(app.get('port'), ()=>{
+    console.log('Servidor ejecutandose en el puerto' + app.get('port'));
+});
